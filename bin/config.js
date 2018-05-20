f3fa5aa204e10b863064a3888bf1a7a4ebe9eaa2
ItaiZ <itaiez@gmail.com>
2018-05-20 23:38:16 +0300

    Update config.js
    change 2

--- bin/config.js
+++ bin/config.js
@@ -14,7 +14,7 @@ var autoconfig  = function (config_overrides){
 
   var ws_info = {
     id: "nationalparks-js",
-    displayName: "Itai's National Parks (JS)",
+    displayName: "Itai National Parks (JS)",
     type: "cluster",
     center: {'latitude': '47.039304', 'longitude': '14.505178'},
     zoom: 4
